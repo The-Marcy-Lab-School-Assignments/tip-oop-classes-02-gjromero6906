8f150138abb4cc785debd713f3e67c398393c9ae
gjromero6906 <gjr6906@gmail.com>
2025-11-18 12:24:21 -0500

    before review

--- index.js
+++ index.js
@@ -6,6 +6,38 @@
 
 class Song {
   //write your code here
+  static playlist = [];
+  #playCount = 0;
+  #rating = 0;
+  constructor(title,artist){
+    this.title =title;
+    this.artist =artist;
+  }
+  get playCount(){
+    return this.#playCount;
+  }
+  get rating(){
+    return this.#rating;
+  }
+  play(){
+    this.#playCount++;
+    console.log(`Now playing: ${this.title} by ${this.artist}`)
+  }
+  rate(stars){
+    if(stars >=1 && stars<=5){
+      this.#rating = stars;
+      console.log(`You rated ${title} ${stars} stars`);
+    }
+  }
+  isPopular(){
+    return this.#playCount>=10?true:false;
+  }
+  static getTotalSongs(){
+    return this.playlist.length;
+  }
+  static findByTitle(title){
+    return this.playlist.find(title);
+  }
 }
 
 // Export the Song class for testing
